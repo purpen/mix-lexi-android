51ac3d366065427fea8eed5bece63ddedd325aaa
lilin <1059232202@qq.com>
2018-06-26 15:17:42 +0800

    已喜欢UI

--- app/src/main/java/com/thn/lexi/MainFragment3.kt
+++ app/src/main/java/com/thn/lexi/MainFragment3.kt
@@ -1,16 +1,17 @@
 package com.thn.lexi
 
-import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
+import android.animation.ObjectAnimator
+import android.support.v7.widget.RecyclerView
+import android.widget.FrameLayout
 import com.basemodule.tools.Constants
 import com.basemodule.tools.ToastUtil
 import com.basemodule.tools.WaitingDialog
 import com.basemodule.ui.BaseFragment
+import com.basemodule.ui.CustomFragmentPagerAdapter
 import com.thn.lexi.goods.GoodsData
-import com.thn.lexi.mine.MineContract
-import com.thn.lexi.mine.MineFavoritesAdapter
-import com.thn.lexi.mine.MinePresenter
+import com.thn.lexi.mine.*
 import kotlinx.android.synthetic.main.fragment_main3.*
+import kotlinx.android.synthetic.main.view_mine_head.*
 
 class MainFragment3 : BaseFragment(), MineContract.View {
     private val dialog: WaitingDialog? by lazy { WaitingDialog(activity) }
@@ -26,20 +27,37 @@ class MainFragment3 : BaseFragment(), MineContract.View {
 
     override val layout: Int = R.layout.fragment_main3
 
+
+    private fun setUpViewPager() {
+        val fragments = ArrayList<BaseFragment>()
+
+        fragments.add(FragmentFavorite.newInstance())
+        fragments.add(FragmentClassify.newInstance())
+
+        val titles = resources.getStringArray(R.array.strings_main_titles)
+        val adapter = CustomFragmentPagerAdapter(childFragmentManager, fragments, titles)
+        customViewPager.adapter = adapter
+        customViewPager.offscreenPageLimit = fragments.size
+        customViewPager.setPagingEnabled(true)
+        slidingTabLayout.setViewPager(customViewPager)
+    }
+
+
     override fun initView() {
+        setUpViewPager()
         this.presenter = MinePresenter(this)
         adapter0 = MineFavoritesAdapter(R.layout.layout_goods_adapter)
-
-        val headView = LayoutInflater.from(activity).inflate(R.layout.view_mine_head, null)
-
-        adapter0.addHeaderView(headView)
-        swipeRefreshLayout.setColorSchemeColors(resources.getColor(R.color.color_6ed7af))
-        swipeRefreshLayout.isRefreshing = false
-        val linearLayoutManager = LinearLayoutManager(activity)
-        recyclerView0.setHasFixedSize(true)
-        recyclerView0.layoutManager = linearLayoutManager
-        recyclerView0.adapter = adapter0
-        recyclerView0.addItemDecoration(RecyclerViewDivider(AppApplication.getContext(), LinearLayoutManager.VERTICAL, resources.getDimensionPixelSize(R.dimen.dp10), resources.getColor(R.color.color_d1d1d1)))
+//
+//        val headView = LayoutInflater.from(activity).inflate(R.layout.view_mine_head, null)
+//
+//        adapter0.addHeaderView(headView)
+//        swipeRefreshLayout.setColorSchemeColors(resources.getColor(R.color.color_6ed7af))
+//        swipeRefreshLayout.isRefreshing = false
+//        val linearLayoutManager = LinearLayoutManager(activity)
+//        recyclerView0.setHasFixedSize(true)
+//        recyclerView0.layoutManager = linearLayoutManager
+//        recyclerView0.adapter = adapter0
+//        recyclerView0.addItemDecoration(RecyclerViewDivider(AppApplication.getContext(), LinearLayoutManager.VERTICAL, resources.getDimensionPixelSize(R.dimen.dp10), resources.getColor(R.color.color_d1d1d1)))
     }
 
 
@@ -48,14 +66,7 @@ class MainFragment3 : BaseFragment(), MineContract.View {
     }
 
     override fun installListener() {
-        swipeRefreshLayout.setOnRefreshListener {
-            adapter0.setEnableLoadMore(false)
-            loadData()
-        }
 
-        adapter0.setOnLoadMoreListener({
-            presenter.loadMoreData("", page)
-        }, recyclerView0)
     }
 
     override fun loadData() {
@@ -64,7 +75,6 @@ class MainFragment3 : BaseFragment(), MineContract.View {
     }
 
     override fun setNewData(data: List<GoodsData.DataBean.ProductsBean>) {
-        swipeRefreshLayout.isRefreshing = false
         adapter0.setNewData(data)
         adapter0.setEnableLoadMore(true)
         showEndView()
@@ -89,7 +99,7 @@ class MainFragment3 : BaseFragment(), MineContract.View {
 
 
     override fun showLoadingView() {
-        if (!swipeRefreshLayout.isRefreshing) dialog?.show()
+        dialog?.show()
     }
 
     override fun dismissLoadingView() {
@@ -97,12 +107,21 @@ class MainFragment3 : BaseFragment(), MineContract.View {
     }
 
     override fun showError(string: String) {
-        swipeRefreshLayout.isRefreshing = false
         ToastUtil.showError(string)
     }
 
     override fun goPage() {
 
+    }
+
+    fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+
+    }
+
+    private var sumY: Float = 0f
+    fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+        sumY += dy
+        relativeLayout.translationY = -Math.min(sumY, resources.getDimensionPixelSize(R.dimen.dp238).toFloat())
     }
 
 }
