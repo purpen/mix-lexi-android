211fd70e18e44af0f4448f110bd96c023e44d3a2
lilin <1059232202@qq.com>
2018-08-10 19:16:49 +0800

    完成探索价格设置

--- app/src/main/java/com/thn/lexi/goods/explore/EditorRecommendAdapter.kt
+++ app/src/main/java/com/thn/lexi/goods/explore/EditorRecommendAdapter.kt
@@ -36,11 +36,17 @@ class EditorRecommendAdapter(layoutResId: Int) : BaseQuickAdapter<EditorRecommen
         GlideUtil.loadImageWithRadius(item.cover,imageView,DimenUtil.getDimensionPixelSize(R.dimen.dp4))
 
         helper.setText(R.id.textViewTitle,item.name)
-        helper.setText(R.id.textViewPrice,item.min_sale_price)
 
-        val textViewOldPrice = helper.getView<TextView>(R.id.textViewOldPrice)
-        textViewOldPrice.visibility = View.VISIBLE
-        textViewOldPrice.paint.flags = Paint.STRIKE_THRU_TEXT_FLAG or Paint.ANTI_ALIAS_FLAG
-        textViewOldPrice.text = "￥" + item.min_sale_price
+        val textViewOldPrice =helper.getView<TextView>(R.id.textViewOldPrice)
+
+        if (item.real_sale_price ==0.0){ //折扣价为0,显示真实价格
+            helper.setText(R.id.textViewPrice, "${item.real_price}")
+            textViewOldPrice.visibility = View.GONE
+        }else{ //折扣价不为0显示折扣价格和带划线的真实价格
+            textViewOldPrice.visibility = View.VISIBLE
+            helper.setText(R.id.textViewPrice, "${item.real_sale_price}")
+            textViewOldPrice.paint.flags = Paint.STRIKE_THRU_TEXT_FLAG or Paint.ANTI_ALIAS_FLAG
+            textViewOldPrice.text = "￥" + item.real_price
+        }
     }
 }
