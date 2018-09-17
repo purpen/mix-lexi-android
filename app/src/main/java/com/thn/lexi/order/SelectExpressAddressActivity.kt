29bf1a8c58c9293781521d75f6ffb2f4f07f13a6
lilin <1059232202@qq.com>
2018-09-17 11:12:50 +0800

    完成店铺优惠券界面

--- app/src/main/java/com/thn/lexi/order/SelectExpressAddressActivity.kt
+++ app/src/main/java/com/thn/lexi/order/SelectExpressAddressActivity.kt
@@ -117,6 +117,8 @@ class SelectExpressAddressActivity : BaseActivity(), SelectExpressAddressContrac
         var hasChecked = false
         for (item in data) {
             if (item.is_default) {
+                //设置默认地址rid
+                createOrderBean.address_rid = item.rid
                 hasChecked = true
                 break
             }
