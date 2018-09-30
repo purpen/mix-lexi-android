4687ac95991d6edff3e1b9f6df3fc282f922cd84
lilin <1059232202@qq.com>
2018-09-30 18:25:11 +0800

    查看全部用户喜欢

---
+++ app/src/main/java/com/thn/lexi/mine/enshrine/recentLook/AllRecentLookGoodsModel.kt
@@ -0,0 +1,52 @@
+package com.thn.lexi.mine.enshrine.recentLook
+
+import com.basemodule.ui.IDataSource
+import com.thn.lexi.net.ClientParamsAPI
+import com.thn.lexi.net.HttpRequest
+import com.thn.lexi.net.URL
+import java.io.IOException
+
+open class AllRecentLookGoodsModel {
+
+    fun loadData(page: Int, sortType: String, minePrice: String, maxPrice: String, cids: String,is_free_postage: String, is_preferential: String,is_custom_made: String,sort_newest:String,callBack: IDataSource.HttpRequestCallBack) {
+
+        val params = ClientParamsAPI.getAllEditorRecommendParams(page, sortType,minePrice, maxPrice, cids,is_free_postage,is_preferential,is_custom_made,sort_newest)
+
+        HttpRequest.sendRequest(HttpRequest.GET, URL.RECENT_LOOK_GOODS, params, object : IDataSource.HttpRequestCallBack {
+            override fun onStart() {
+                callBack.onStart()
+            }
+
+            override fun onSuccess(json: String) {
+                callBack.onSuccess(json)
+            }
+
+            override fun onFailure(e: IOException) {
+                callBack.onFailure(e)
+            }
+        })
+    }
+
+    /**
+     * 获取商品分类
+     */
+    fun getGoodsClassify(callBack: IDataSource.HttpRequestCallBack) {
+        val params = ClientParamsAPI.getDefaultParams()
+        HttpRequest.sendRequest(HttpRequest.GET, URL.GOODS_CATEGORIES_URL, params, object : IDataSource.HttpRequestCallBack {
+            override fun onStart() {
+                callBack.onStart()
+            }
+
+            override fun onSuccess(json: String) {
+                callBack.onSuccess(json)
+            }
+
+            override fun onFailure(e: IOException) {
+                callBack.onFailure(e)
+            }
+        })
+    }
+
+}
+
+
