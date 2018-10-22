51d1431615b0badcab4698f448cd085bea24bb01
lilin <1059232202@qq.com>
2018-10-22 14:43:34 +0800

    合并文章详情的footerview

--- app/src/main/java/com/lexivip/lexi/index/lifehouse/FragmentLifeHouse.kt
+++ app/src/main/java/com/lexivip/lexi/index/lifehouse/FragmentLifeHouse.kt
@@ -2,7 +2,6 @@ package com.lexivip.lexi.index.lifehouse
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.graphics.Rect
 import android.net.Uri
