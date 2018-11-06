4c024fbcca51b65d7f28cd3b9bd92509846e369b
lilin <1059232202@qq.com>
2018-11-06 16:15:38 +0800

    处理商品详情图片变形

--- app/src/main/java/com/lexivip/lexi/index/detail/GoodsDetailActivity.kt
+++ app/src/main/java/com/lexivip/lexi/index/detail/GoodsDetailActivity.kt
@@ -273,7 +273,7 @@ class GoodsDetailActivity : BaseActivity(), GoodsDetailContract.View, View.OnCli
         designPavilionProductAdapter.setNewData(imgUrls)
 
         //跳转品牌馆商品详情
-        designPavilionProductAdapter.setOnItemClickListener { _, view, position ->
+        designPavilionProductAdapter.setOnItemClickListener { _, _, position ->
             val productBean = data.products[position]
             val intent = Intent(this, GoodsDetailActivity::class.java)
             intent.putExtra(GoodsDetailActivity::class.java.simpleName, productBean)
