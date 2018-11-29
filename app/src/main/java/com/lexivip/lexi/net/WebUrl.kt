b9861b831a27a17594f13e3298dabe65c8e328ab
wyl <m18739429102@163.com>
2018-11-29 17:18:28 +0800

    分享链接添加
    Signed-off-by: wyl <m18739429102@163.com>

--- app/src/main/java/com/lexivip/lexi/net/WebUrl.kt
+++ app/src/main/java/com/lexivip/lexi/net/WebUrl.kt
@@ -2,5 +2,8 @@ package com.lexivip.lexi.net
 
 object WebUrl{
     const val BASE_URL="https://h5.lexivip.com/"
-    const val LIFE="${BASE_URL}"
+    const val LIFE="${BASE_URL}article/life?rid="
+    const val GRADD="${BASE_URL}article/grass?rid="
+    const val USER="${BASE_URL}user/home?uid="
+    const val GOODS="${BASE_URL}product_view?rid="
 }
